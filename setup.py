188fd05e49cef6ddf3baaafb22b03fd7a9c028a5
fred <fred.battista@qntfy.com>
2019-06-11 10:12:23 -0700

    Rolling back cython version to d3m base package version

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     classifiers=[],
     packages=find_packages(),
     install_requires=[
-        'cython==0.29.3',
+        'cython==0.29.7',
         'tqdm',
         'numpy',
         'pandas',
         'lap @ git+https://github.com/nowfred/lap.git@v0.5.0#egg=lap',
         'lapjv @ git+https://github.com/nowfred/lapjv.git@v1.4.1#egg=lapjv'
     ],
-    version="1.0.1"
+    version="1.0.2"
 )
